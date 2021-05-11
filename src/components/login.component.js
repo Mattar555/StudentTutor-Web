b79372c145822ef53e2ed1d7599464271ea986bd
Marwan <marwan@Marwans-MacBook-Pro.local>
2021-05-11 16:01:34 +1000

    Styling changes/refactor

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -5,38 +5,38 @@ export default class Login extends Component {
     render() {
         return (
             <div className="auth-wrapper">
-            <form>
-                <h3>Sign In</h3>
+                <form>
+                    <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <br />
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <br />
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
                     </div>
-                </div>
 
-                <br />
-        
-                <Link to="/welcome" className="btn btn-primary btn-block">Submit</Link>
-                {/* <button type="submit" className="btn btn-primary btn-block">Submit</button> */}
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
+                    <br />
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <br />
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <br />
+            
+                    <Link to="/welcome" className="btn btn-primary btn-block">Submit</Link>
+                    {/* <button type="submit" className="btn btn-primary btn-block">Submit</button> */}
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </form>
             </div>
         );
     }
