55862e848cbb1f1b7684b5fd22bcc0bd3f81ea99
Marwan <marwan@Marwans-MacBook-Pro.local>
2021-05-12 13:19:45 +1000

    Added redirect on state change

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,22 +1,61 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom"
 
-export default class Login extends Component {
-    render() {
+export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+
+    const validate = async () => {
+        let credentials = {
+            username: email,
+            password: password
+        }
+        let url = 'http://localhost:8080/demo/validate'
+        try {
+            const response = await fetch(url, {
+                method: 'put',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(credentials)
+            });
+            setRedirect(true);
+            //resolve();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    if (redirect) {
+        return <Redirect to="/welcome"/>
+    }
+
         return (
+
             <div className="auth-wrapper">
-                <form>
+                <form onSubmit>
                     <h3>Sign In</h3>
                     <div className="form-group">
                         <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
+                        <input 
+                            type="email" 
+                            className="form-control"
+                             placeholder="Enter email" 
+                             value={email} 
+                             onChange={(e) => setEmail(e.target.value)}/>
                     </div>
 
                     <br />
 
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
                     </div>
 
                     <br />
@@ -38,5 +77,4 @@ export default class Login extends Component {
                 </form>
             </div>
         );
-    }
 }
